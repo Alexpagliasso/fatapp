ffc00c90c92e393a54b99eb13e9b6025b2dfe360
Alexpagliasso <alexpagliasso@gmail.com>
2022-03-04 17:44:55 +0100

    aggiunta slider crea rosa

--- src/componenti/creaRosa/CreaRosa.js
+++ src/componenti/creaRosa/CreaRosa.js
@@ -4,6 +4,11 @@ import data from "../../Json/mockup.json";
 import Team from "./Team/Team";
 import Players from "../Players/Players";
 import { useState } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+// Import Swiper styles
+import 'swiper/css';
 
 function CreaRosa() {
   const Data = data.slice(0, 10);
@@ -13,9 +18,7 @@ function CreaRosa() {
     for (let i = 0; i < Data.length; i++) {
       if (event.target.id === Data[i].team_key) {
         let players = Data[i].players.map((item) => item);
-
-          setPlayers(players);
-
+        setPlayers(players);
       }
     }
   };
@@ -30,9 +33,20 @@ function CreaRosa() {
         })}
       </div>
       <div className="containerTeams">
-        {players.map((player) => {
-          return <Players key={player.player_key} name={player.player_name} image={player.player_image}  />;
-        })}
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}>
+          {players.map((player) => {
+            return (
+
+              <SwiperSlide>
+                <Players key={player.player_key} name={player.player_name} image={player.player_image} />
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
       </div>
     </div>
   );
